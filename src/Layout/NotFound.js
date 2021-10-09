b633b1e754702102ad723c2b64231d60d76d1958
carlosfierro207 <75703587+carlosfierro207@users.noreply.github.com>
2021-10-08 20:06:49 -0500

    second commit

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -1,9 +1,8 @@
 import React from "react";
-import{Link} from "react-router-dom"
+
 function NotFound() {
   return (
     <div className="NotFound">
-      <Link to="/">Go Home</Link>
       <h1>Not Found</h1>
     </div>
   );
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,81 +1,51 @@
-
-import Header from "./HeaderAndBreadCrumb/Header";
-import React, { Fragment, useState, useEffect } from "react";
-import { Switch, Route, useHistory } from "react-router-dom";
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "../Home";
+import Study from "../Deck/Study";
 import NotFound from "./NotFound";
-import Decks from "./Decks";
-import Form from "./Form";
-import BreadCrumb from "./HeaderAndBreadCrumb/BreadCrumb";
-import {createDeck, listDecks} from "../utils/api";
-import ViewDecks from "./DecksAndCards/ViewDecks"
+import Header from "./Header";
+import DeckEdit from "../Deck/Edit";
+import DeckView from "../Deck/View";
+import CardEdit from "../Card/Edit";
+import CardCreate from "../Card/Create";
+import DeckCreate from "../Deck/Create";
 
 function Layout() {
-  
-  const [decks, setDecks] = useState([]);
-  const abortController = new AbortController();
-  const history = useHistory();
-  const signal = abortController.signal;
-  useEffect(() => {
-		getDeck();
-
-		return () => {
-			abortController.abort();
-		};
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [decks.length]);
-  async function getDeck() {
-		try {
-			const response = await listDecks(signal);
-			setDecks(response);
-		}
-		catch(error) {
-			if(error.name !== "AbortError") {
-				throw error;
-			}
-		}
-	}
-
-  async function addDeck(deck){
-    const done = await createDeck(deck, signal);
-    getDeck();
-    return done.id;
-  }
-
-
-  async function deleteDeck(id){
-    if(window.confirm(`Delete this deck?\nYou will not be able to recover it.`)){
-      await deleteDeck(id, signal);
-      getDeck();
-      history.push("/");
-    }
-  }
-
-  
-
   return (
-    <Fragment>
+    <>
       <Header />
-      <div class="container">
-      
-      <Switch>
-        <Route exact path="/">
-          <Decks decks={decks} deleteDeck={deleteDeck} getDeck={getDeck}/>
-        </Route>
-        <Route path="/decks/new">
-          
-          <Form type="deck" edit={false} decks={decks} getDeck={getDeck} createDeck={addDeck} signal={signal} />
-        </Route>
-        <Route path="/decks/:deckId">
-          
-          <ViewDecks decks={decks} signal={signal} getDeck={getDeck}/>
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
+      <div className="container">
+        <Switch>
+          <Route path="/decks/new">
+            <DeckCreate />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <DeckEdit />
+          </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <CardCreate />
+          </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <CardEdit />
+          </Route>
+          <Route exact={true} path="/decks/:deckId">
+            <DeckView />
+          </Route>
+          <Route exact={true} path="/decks">
+            <Redirect to="/" />
+          </Route>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
-    </Fragment>
+    </>
   );
 }
 
